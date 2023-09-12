0ce8e10a8c47e3ce507aa950cb1f1ef71848f6a8
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-09-12 11:54:34 +0700

    Create new style

--- src/views/sub-pages/stock/item/DetailItem.js
+++ src/views/sub-pages/stock/item/DetailItem.js
@@ -1,5 +1,5 @@
 // ** React Import
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 // ** Mui Import
 import {
@@ -10,6 +10,7 @@ import {
   Checkbox,
   Collapse,
   Divider,
+  Grid,
   IconButton,
   TextField,
   TextareaAutosize,
@@ -31,78 +32,75 @@ const DetailItem = ({ dataRow }) => {
     console.log('Checkbox ถูกเปลี่ยนแปลงเป็น:', event.target.checked)
   }
 
+  useEffect(() => {
+    console.log('dataRow', dataRow)
+  }, [dataRow])
+
+  const checkboxStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
+
   return (
     <Box>
-      <Box sx={{ display: 'flex' }}>
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <Box>
-            <Typography sx={{ margin: 1 }}>Item Name :</Typography>
-            <TextField size='small' variant='filled' value={dataRow.item_name} />
-          </Box>
-          <Box sx={{ mt: 4 }}>
-            <Typography sx={{ marginBottom: 2 }}>Item Group :</Typography>
-            <TextField size='small' variant='filled' label='' value={dataRow.item_group} />
-          </Box>
-        </Box>
-
-        <Box sx={{ ml: 18, display: 'flex', flexDirection: 'column' }}>
-          <Box sx={{ display: 'flex' }}>
-            <Checkbox {...label} checked={dataRow.disabled} onChange={handleCheckboxChange} />
-            <Typography variant='subtitle2' sx={{ mt: 2 }}>
-              Disabled
-            </Typography>
-          </Box>
-          <Box sx={{ display: 'flex' }}>
-            <Checkbox {...label} checked={dataRow.allow_alternative_item} onChange={handleCheckboxChange} />
-            <Typography variant='subtitle2' sx={{ mt: 2 }}>
-              Allow Alternative Item
-            </Typography>
-          </Box>
-          <Box sx={{ display: 'flex' }}>
-            <Checkbox {...label} checked={dataRow.is_stock_item} onChange={handleCheckboxChange} />
-            <Typography variant='subtitle2' sx={{ mt: 2 }}>
-              Maintain Stock
-            </Typography>
-          </Box>
-          <Box sx={{ display: 'flex' }}>
-            <Checkbox {...label} checked={dataRow.has_variants} onChange={handleCheckboxChange} />
-            <Typography variant='subtitle2' sx={{ mt: 2 }}>
-              Has Variants
-            </Typography>
-          </Box>
-        </Box>
-      </Box>
-      <Box sx={{ display: 'flex', mt: 6 }}>
-        <Box>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Typography sx={{ margin: 1 }}>Item Name :</Typography>
+          <TextField size='small' variant='filled' value={dataRow.item_name} />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography sx={{ marginBottom: 2 }}>Item Group :</Typography>
+          <TextField size='small' variant='filled' label='' value={dataRow.item_group} />
+        </Grid>
+        <Grid item xs={12} sm={6} sx={checkboxStyle}>
+          <Checkbox {...label} checked={dataRow.disabled === 1 ? true : false} onChange={handleCheckboxChange} />
+          <Typography variant='subtitle2'>Disabled</Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} sx={checkboxStyle}>
+          <Checkbox
+            {...label}
+            checked={dataRow.allow_alternative_item === 1 ? true : false}
+            onChange={handleCheckboxChange}
+          />
+          <Typography variant='subtitle2'>Allow Alternative Item</Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} sx={checkboxStyle}>
+          <Checkbox {...label} checked={dataRow.is_stock_item === 1 ? true : false} onChange={handleCheckboxChange} />
+          <Typography variant='subtitle2'>Maintain Stock</Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} sx={checkboxStyle}>
+          <Checkbox {...label} checked={dataRow.has_variants === 1 ? true : false} onChange={handleCheckboxChange} />
+          <Typography variant='subtitle2'>Has Variants</Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} sx={checkboxStyle}>
           <Typography sx={{ marginBottom: 2 }}>Default Unit of Measure :</Typography>
           <TextField size='small' variant='filled' label='' value={dataRow.stock_uom} />
-        </Box>
-        <Box sx={{ ml: 20 }}>
+        </Grid>
+        <Grid item xs={12} sm={6}>
           <Typography sx={{ marginBottom: 2 }}>Valuation Rate :</Typography>
           <TextField size='small' variant='filled' label='' value={dataRow.valuation_rate} />
           <Box sx={{ display: 'flex' }}>
-            <Checkbox {...label} checked={dataRow.is_fixed_asset} onChange={handleCheckboxChange} />
+            <Checkbox
+              {...label}
+              checked={dataRow.is_fixed_asset === 1 ? true : false}
+              onChange={handleCheckboxChange}
+            />
             <Typography sx={{ m: 2 }}>Is Fixed Asset</Typography>
           </Box>
-        </Box>
-      </Box>
-      <Box sx={{ display: 'flex', mt: 6 }}>
-        <Box sx={{ ml: 82 }}>
-          <Box>
-            <Typography sx={{ marginBottom: 2 }}>Over Delivery/Receipt Allowance (%) :</Typography>
-            <TextField size='small' variant='filled' label='' value={dataRow.over_delivery_receipt_allowance} />
-          </Box>
-          <Box sx={{ mt: 4 }}>
-            <Typography sx={{ marginBottom: 2 }}>Over Billing Allowance (%) :</Typography>
-            <TextField size='small' variant='filled' label='' value={dataRow.over_billing_allowance} />
-          </Box>
-        </Box>
-      </Box>
-      <Box sx={{ mt: 30, display: 'flex' }}>
-        <Button size='small' variant='filled' label='' onClick={handleClickDescription}>
-          <Typography variant='h6'>Description</Typography>
-        </Button>
-        <Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography sx={{ marginBottom: 2 }}>Over Delivery/Receipt Allowance (%) :</Typography>
+          <TextField size='small' variant='filled' label='' value={dataRow.over_delivery_receipt_allowance} />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography sx={{ marginBottom: 2 }}>Over Billing Allowance (%) :</Typography>
+          <TextField size='small' variant='filled' label='' value={dataRow.over_billing_allowance} />
+        </Grid>
+        <Grid item xs={12} sx={{ display: 'flex' }}>
+          <Button size='small' variant='filled' label='' onClick={handleClickDescription}>
+            <Typography variant='h6'>Description</Typography>
+          </Button>
           <CardActions className='card-action-dense'>
             <IconButton size='small' onClick={handleClickDescription}>
               {collapseDescription ? (
@@ -112,37 +110,37 @@ const DetailItem = ({ dataRow }) => {
               )}
             </IconButton>
           </CardActions>
-        </Box>
-      </Box>
-      <Box>
-        <Collapse in={collapseDescription}>
-          <Divider sx={{ margin: 0 }} />
-          <CardContent>
-            <Box>
-              <Typography variant='subtitle2'>Description</Typography>
-              <TextareaAutosize
-                style={{ minHeight: '200px', width: '100%' }}
-                size='small'
-                variant='filled'
-                label=''
-                value={dataRow.description}
-              />
-            </Box>
-            <Box>
-              <Typography variant='subtitle1'>Brand</Typography>
-              <TextField size='small' variant='filled' label='' />
-            </Box>
-          </CardContent>
-        </Collapse>
-      </Box>
-      <Box>
-        <Typography variant='h6' sx={{ m: 2 }}>
-          Add Comment
-        </Typography>
-        <TextField size='small' variant='filled' label='' multiline rows={4} fullWidth />
-        <Typography variant='subtitle2'>Ctrl+Enter to add comment</Typography>
-        <Button>add comment</Button>
-      </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Collapse in={collapseDescription}>
+            <Divider sx={{ margin: 0 }} />
+            <CardContent>
+              <Box>
+                <Typography variant='subtitle2'>Description</Typography>
+                <TextareaAutosize
+                  style={{ minHeight: '200px', width: '100%' }}
+                  size='small'
+                  variant='filled'
+                  label=''
+                  value={dataRow.description}
+                />
+              </Box>
+              <Box>
+                <Typography variant='subtitle1'>Brand</Typography>
+                <TextField size='small' variant='filled' label='' />
+              </Box>
+            </CardContent>
+          </Collapse>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='h6' sx={{ m: 2 }}>
+            Add Comment
+          </Typography>
+          <TextField size='small' variant='filled' label='' multiline rows={4} fullWidth />
+          <Typography variant='subtitle2'>Ctrl+Enter to add comment</Typography>
+          <Button>add comment</Button>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
