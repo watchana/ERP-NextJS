21b1af2d5f33c6bda2136ce105e38c89013599cc
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-09-12 15:52:27 +0700

    Update state in ManufacturingItem

--- src/views/sub-pages/stock/item/ManufacturingItem.js
+++ src/views/sub-pages/stock/item/ManufacturingItem.js
@@ -1,14 +1,16 @@
 // ** React Imports
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 // MUI imports
 import { Box, Typography, Checkbox, Card, Grid } from '@mui/material'
 
 const ManufacturingItem = ({ dataRow }) => {
-  const label = { inputProps: { 'aria-label': 'Checkbox demo' } }
+  const [data, setData] = useState(dataRow)
 
   const handleCheckboxChange = event => {
     console.log('Checkbox ถูกเปลี่ยนแปลงเป็น:', event.target.checked)
+
+    setData({ ...data, [event.target.name]: event.target.checked === true ? 1 : 0 })
   }
 
   return (
@@ -16,12 +18,20 @@ const ManufacturingItem = ({ dataRow }) => {
       <Card>
         <Grid container spacing={2}>
           <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center' }}>
-            <Checkbox {...label} checked={dataRow.include_item_in_manufacturing} onChange={handleCheckboxChange} />
+            <Checkbox
+              checked={data.include_item_in_manufacturing === 1 ? true : false}
+              name='include_item_in_manufacturing'
+              onChange={handleCheckboxChange}
+            />
             <Typography variant='subtitle1'>Include Item In Manufacturing</Typography>
           </Grid>
           <Grid item xs={12} sx={{ display: 'flex', flexDirection: 'column' }}>
             <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-              <Checkbox {...label} checked={dataRow.is_sub_contracted_item} onChange={handleCheckboxChange} />
+              <Checkbox
+                name='is_sub_contracted_item'
+                checked={data.is_sub_contracted_item === 1 ? true : false}
+                onChange={handleCheckboxChange}
+              />
               <Typography variant='subtitle1'>Supply Raw Materials for Purchase</Typography>
             </Box>
             <Box sx={{ ml: 4 }}>
