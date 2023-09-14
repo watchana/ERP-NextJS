08b98968cdbf038bcf1cde7d4f522c2ee15a3101
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-09-14 10:53:27 +0700

    Add Button Create, Input Search ID and Add Column Status with prop docStatusName

--- src/views/sub-pages/ContentLeft.js
+++ src/views/sub-pages/ContentLeft.js
@@ -1,32 +1,73 @@
+// ** React Imports
+import { useState } from 'react'
+
 // ** MUI Imports
-import { Box, Card, Chip, Button, Typography } from '@mui/material'
+import { Box, Card, Chip, Button, Typography, Divider, Grid, TextField } from '@mui/material'
 
 // ** Components
 
-const ContentLeft = ({ menuColumn, dataRow, handleRowClick, doctype }) => {
+const ContentLeft = ({ menuColumn, data, handleRowClick, doctype, docStatusName }) => {
+  const [dataFilter, setDataFilter] = useState(data)
+
+  const handleIDSearch = event => {
+    console.log('Text ถูกเปลี่ยนแปลงเป็น:', event.target.value)
+    const value = event.target.value
+
+    if (value === '') {
+      setDataFilter(data)
+    } else {
+      {
+        // ค้นหาคำที่คล้ายคลึงกับคำที่คุณค้นหา
+        const matches = data.filter(item => item.name.toLowerCase().includes(value.toLowerCase()))
+
+        setDataFilter(matches)
+      }
+    }
+  }
+
   return (
     <Box sx={{ px: 2 }}>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: 3 }}>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2, mb: 5 }}>
         <Typography variant='h6' sx={{ fontWeight: 'bold' }}>
           {doctype}
         </Typography>
+        <Button variant='contained' color='primary' size='small'>
+          Create {doctype}
+        </Button>
       </Box>
-      <Card>
-        {dataRow.map((item, index) => (
-          <Box
-            sx={{ display: 'flex', justifyContent: 'space-between', border: 1 }}
-            key={item.name}
-            onClick={() => handleRowClick(item)}
-          >
-            <Button variant='outlined' fullWidth sx={{ justifyContent: 'space-between', textAlign: 'left' }}>
-              <Typography variant='body2' sx={{ fontWeight: 'bold' }}>
-                {item.name}
-              </Typography>
-              <Chip label={'enable'} size='small' />
-            </Button>
+      <Box sx={{ backgroundColor: 'background.paper', borderTopRightRadius: '10px', borderTopLeftRadius: '10px' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} sx={{ p: 2, ml: 2 }}>
+            <TextField fullWidth variant='outlined' size='small' label='ID Search' onChange={handleIDSearch} />
+          </Grid>
+          <Grid item xs sx={{ p: 2, ml: 2, justifyContent: 'center', display: 'flex' }}>
+            <Button> test </Button>
+          </Grid>
+        </Grid>
+        <Divider />
+        {dataFilter.map((item, index) => (
+          <Box key={item.name}>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                cursor: 'pointer',
+                px: 2
+              }}
+              onClick={() => handleRowClick(item)}
+            >
+              <Typography variant='h6'>{item.name}</Typography>
+              {item[docStatusName] === 0 ? (
+                <Chip label='Enable' color='success' size='small' />
+              ) : (
+                <Chip label='Disable' color='error' size='small' />
+              )}
+            </Box>
+            <Divider />
           </Box>
         ))}
-      </Card>
+      </Box>
     </Box>
   )
 }
